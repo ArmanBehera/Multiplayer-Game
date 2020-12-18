ac7840b2c9c14b28d17e1e1385170e8e74f0bb2e
ArmanBehera <73575356+ArmanBehera@users.noreply.github.com>
2020-12-18 11:59:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+var changingBall, position;
+var database;
+
+function setup(){
+    createCanvas(500,500);
+
+    database = firebase.database();
+
+    changingBall = createSprite(250,250,10,10);
+    changingBall.shapeColor = "red";
+
+    var changingBallPosition = database.ref("ball/position");
+    changingBallPosition.on("value", readPosition, showError);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function readPosition(data){
+    
+    position = data.val();
+    changingBall.x = position.x;
+    changingBall.y = position.y;
+}
+
+function writePosition(x, y){
+
+    database.ref("ball/position").set({
+
+        'x' : position.x + x,
+        'y' : position.y + y 
+    })
+}
+
+function showError(){
+
+    console.log("There is an error in the code");
+}
